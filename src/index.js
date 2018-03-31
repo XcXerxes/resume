e8bd7b033b9d175bc0d3fb3bae0a07c7b2b71ac3
XcXerxes <xcxerxes@aliyun.com>
2018-03-31 19:14:59 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './App'
+import 'animate.css'
+import './assets/scss/index.scss'
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
